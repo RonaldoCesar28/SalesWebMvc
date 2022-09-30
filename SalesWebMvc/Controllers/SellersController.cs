094f266ab28d4fde83d2ea5475d23d4681091090
RonaldoCesar28 <ronaldocesar28@outlook.com>
2022-09-30 11:40:29 -0300

    Update seller and custom service exception

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,41 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
 using SalesWebMvc.Services;
-using SalesWebMvc.Models.ViewModels; 
+using SalesWebMvc.Models.ViewModels;
 using System;
+using System.Collections.Generic;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
-        private readonly DepartmentService _departmentService;   
+        private readonly DepartmentService _departmentService;
 
         public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
-            _departmentService = departmentService; 
+            _departmentService = departmentService;
         }
         public IActionResult Index()
         {
-            var list = _sellerService.FindAll(); 
+            var list = _sellerService.FindAll();
 
-            return View(list); 
+            return View(list);
         }
 
         public IActionResult Create()
         {
             var departments = _departmentService.FindAll();
-            var viewModel = new SellerFormViewModel { Departments = departments }; 
-            return View(viewModel);   
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken] 
-        public IActionResult Create(Seller seller) 
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
-            return RedirectToAction(nameof(Index)); 
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Delete(int? id)
@@ -45,13 +47,13 @@ namespace SalesWebMvc.Controllers
                 return NotFound();
             }
 
-            var obj = _sellerService.FindById(id.Value); 
+            var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound(); 
+                return NotFound();
             }
 
-            return View(obj);  
+            return View(obj);
         }
 
         [HttpPost]
@@ -59,7 +61,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult Delete(int id)
         {
             _sellerService.Remove(id);
-            return RedirectToAction(nameof(Index));  
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Details(int? id)
@@ -75,7 +77,49 @@ namespace SalesWebMvc.Controllers
                 return NotFound();
             }
 
-            return View(obj); 
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound(); 
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();  
+            }
+           
         }
     }
 }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services
 {
@@ -13,30 +14,47 @@ namespace SalesWebMvc.Services
 
         public SellerService(SalesWebMvcContext context)
         {
-            _context = context; 
+            _context = context;
         }
 
         public List<Seller> FindAll()
         {
-            return _context.Seller.ToList(); 
+            return _context.Seller.ToList();
         }
 
         public void Insert(Seller obj)
-        {            
-            _context.Add(obj);  
-            _context.SaveChanges(); 
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
         }
 
         public Seller FindById(int id)
         {
-            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id); 
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
         {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
-            _context.SaveChanges();  
+            _context.SaveChanges();
         }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        } 
     }
 }
